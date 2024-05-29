df69042d08a2acd05132e4cc854c02f2c45d0a72
Raghu Siddagalla <111729590+raghusiddagalla378@users.noreply.github.com>
2024-05-29 12:24:29 +0530

    Add files via upload

---
+++ JavaScript-28-05-24-day7/script.js
@@ -0,0 +1,65 @@
+// // string consructor declaration
+// let name= new String("raghu siddagalla");//using with new keyword 
+// console.log(name);
+
+// let names=String("jessy"); //without using new keyword
+// console.log(names);
+
+// // string positions
+// let greeting="Good morning";
+// let numStr="raghu 9121815478"
+// console.log(greeting[0]);
+// console.log(greeting[3]);
+// console.log(greeting[6]);
+// console.log(greeting.length);
+// console.log(numStr[7]);
+// console.log(numStr.length);
+
+// // using for loop to print individual characters
+// for(i=0;i<=numStr.length;i++){
+//     console.log(numStr[i]);
+// }
+
+// // reverse a string
+// for(i=numStr.length-1;i>=0;i--){
+//     console.log(numStr[i]);
+// }
+
+
+
+
+let arrayNames=["raghu","shiva","rambabu","dileep"];
+let i=0; 
+// let getname=[];
+while(i<=arrayNames.length){
+    console.log(arrayNames[i]);
+    i++;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
