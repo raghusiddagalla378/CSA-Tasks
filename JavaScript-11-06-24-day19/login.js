eda349184991a6d444ef5082037924a0f304910f
Raghu Siddagalla <111729590+raghusiddagalla378@users.noreply.github.com>
2024-06-12 11:52:13 +0530

    Delete JavaScript-11-06-24-day19 directory

--- JavaScript-11-06-24-day19/login.js
+++
@@ -1,9 +0,0 @@
-
-import {person} from './export.js';
-import { person as myname } from './export.js';
-let {name,age,schoolname}=person;
-console.log(person);
-// console.log(person.name);
-console.log(myname);
-
-
